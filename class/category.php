2d27817f9cbb013bdef6bbcbe96d697782a4efd0
jackyyrose <jack.xu.nz@gmail.com>
2016-06-15 09:12:29 +1200

    add getParentId & getSiblingIds function

--- class/category.php
+++ class/category.php
@@ -42,5 +42,40 @@ class Category
         
         return $children;
     }
+    
+    function getParentId()
+    {
+        $parentId = 0;
+        
+        require("./common/dblogin.php");
+        $conn   = new mysqli($servername, $username, $password, $dbname);
+        $sql    = "SELECT parent_id FROM file where id = " . $this->cid;
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row      = $result->fetch_assoc();
+            $parentId = $row["parent_id"];
+        }
+        $conn->close();
+        
+        return $parentId;
+    }
+    
+    function getSiblingIds()
+    {
+        $sibling = array();
+        
+        require("./common/dblogin.php");
+        $conn   = new mysqli($servername, $username, $password, $dbname);
+        $sql    = "SELECT id FROM file where parent_id = " . $this->getParentId();
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                array_push($sibling, $row["id"]);
+            }
+        }
+        $conn->close();
+        
+        return $sibling;
+    }
 }
 ?>
